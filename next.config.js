72080d982292afc58beaa5276020a76086f135ef
dodogr8 <dodogr8@example.com>
2026-07-26 06:07:17 +0700

    fix: unified config for ESM, postcss and path aliases

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone", // 💡 ຫຼຸດ Thread/Process ໃຫ້ເບົາບາງທີ່ສຸດ
   typescript: {
     ignoreBuildErrors: true,
   },
